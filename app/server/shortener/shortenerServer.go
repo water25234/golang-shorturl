0bae6ae20c359ea935cf5241db1c61267ee14a8a
water25234 <water25234@gmail.com>
2020-07-18 15:21:24 +0800

    fix num length

--- app/server/shortener/shortenerServer.go
+++ app/server/shortener/shortenerServer.go
@@ -24,7 +24,7 @@ func SaveShortenerURL(Url string) interface{} {
 
 	getLastShortenerID, err := modelshortener.GetLastShortenerID()
 
-	num = getLastShortenerID.ShortenerID + 1000
+	num = getLastShortenerID.ShortenerID + 1000000000
 
 	shortenerEncodeNum := Encode(num)
 
